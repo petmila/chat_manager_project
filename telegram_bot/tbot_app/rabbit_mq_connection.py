21bedb90d6b5b36d618fe0842db500c9ee793f82
petmila <mila-petrenko@inbox.ru>
2025-04-16 23:58:21 +0300

    add connection ensure

--- telegram_bot/tbot_app/rabbit_mq_connection.py
+++ telegram_bot/tbot_app/rabbit_mq_connection.py
@@ -16,8 +16,14 @@ class RabbitMQConnection:
         self.channel = self.connection.channel()
         self.channel.confirm_delivery()
 
+    def _ensure_connection(self):
+        """Переподключение при закрытом соединении"""
+        if not self.connection or self.connection.is_closed:
+            self.connect()
+
     def send_message(self, queue_name, message):
         """Отправляет сообщение в очередь"""
+        self._ensure_connection()
         self.channel.queue_declare(queue=queue_name, durable=True)
         self.channel.basic_publish(
             exchange="",
